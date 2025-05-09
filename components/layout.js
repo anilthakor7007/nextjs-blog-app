1c09c1053dbe5c1dbb25526149939ee55f39bdf9
anil thakor <anilthakor7007@gmail.com>
2025-05-09 13:12:43 +0530

    name changes

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Anil Thakor';
+const name = 'Anil Thakor Saheb';
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
